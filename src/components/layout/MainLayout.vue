c2b08ba3b2c34ace44623f39cebc3f643792394c
yaseliwanoff <yselivanov857@gmail.com>
2025-12-07 18:46:26 +0300

    [create]: main layout file in all project interface

---
+++ src/components/layout/MainLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from '../ui/header/Header.vue';
+import Footer from '../ui/footer/Footer.vue';
+
+import Home from '../../pages/Home.vue';
+</script>
+
+<template>
+    <Header />
+    <main class="container">
+        <Home />
+    </main>
+    <Footer />
+</template>
+
+<style scoped>
+    .container {
+        max-width: 1148px;
+        width: 100%;
+        padding: 0px 40px;
+        margin: 0 auto;
+    }
+</style>
