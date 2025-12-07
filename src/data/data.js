1008ddfd8ffef1a624784bf6567a6a7371cdb4e7
yaseliwanoff <yselivanov857@gmail.com>
2025-12-07 18:44:58 +0300

    [create]: main data file

---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+export const addsListForSlider = [
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+    { "name": "slider 1", "image": "none" },
+]
