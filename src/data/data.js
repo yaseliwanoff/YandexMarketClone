f64645a34c3c4b0bad3418817339afd6fb84dbc6
= <yselivanov857@gmail.com>
2025-12-08 13:44:04 +0300

    [update]: global meta

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,55 @@
+import AlfaBannerAdd1 from "../../public/adds/1alfa.jpg";
+import AlfaBannerAdd2 from "../../public/adds/alfa.jpg";
+
 export const addsListForSlider = [
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-    { "name": "slider 1", "image": "none" },
-]
+  {
+    name: "slider 1",
+    image: AlfaBannerAdd1,
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 2",
+    image: AlfaBannerAdd2,
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 3",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 4",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 5",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 6",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 7",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 8",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 9",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+  {
+    name: "slider 10",
+    image: "https://placecats.com/680/283",
+    link: "https://google.com/",
+  },
+];
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,9 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import Home from "../pages/Home.vue";
 
-const routesList = [
-    { path: "/", component: Home },
-];
+const routesList = [{ path: "/", component: Home }];
 
 export const router = createRouter({
-    routes: routesList,
-    history: createMemoryHistory(),
+  routes: routesList,
+  history: createMemoryHistory(),
 });
